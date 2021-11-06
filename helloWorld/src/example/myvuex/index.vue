7e6533ace2531b433cee57e398dbd75176546102
keshanwen <624953505@qq.com>
2021-11-06 22:12:41 +0800

    feat:模块化代码实例

--- helloWorld/src/example/myvuex/index.vue
+++ helloWorld/src/example/myvuex/index.vue
@@ -1,34 +1,65 @@
 <template>
-   <div>
-     i am vuex demo
-     <h1>
-       {{ this.$store.state.num }}
-     </h1>
-     <button @click="add">点我</button>
-      <p>{{this.$store.state.age}}</p>
-    <button @click="myFn2">我是按钮2</button>
-   </div>
+  <div id="app">
+    <p>{{this.$store.state.globalName}}</p>
+    <p>{{this.$store.getters.getGlobalName}}</p>
+    <button @click="globalFn1">同步操作</button>
+    <button @click="globalFn2">异步操作</button>
+    <hr>
+    <p>{{this.$store.state.home.name}}</p>
+    <!--注意点: 如果获取的是模块中state共享的数据, 那么需要加上模块的名称-->
+    <!--注意点: 如果获取的是模块中getters共享的数据, 那么不需要加上模块的名称-->
+    <p>{{this.$store.getters.getHomeName}}</p>
+    <button @click="homeFn1">同步操作</button>
+    <button @click="homeFn2">异步操作</button>
+    <hr>
+    <p>{{this.$store.state.account.name}}</p>
+    <p>{{this.$store.getters.getAccountName}}</p>
+    <button @click="accountFn1">同步操作</button>
+    <button @click="accountFn2">异步操作</button>
+    <hr>
+    <p>{{this.$store.state.account.login.name}}</p>
+    <p>{{this.$store.getters.getLoginName}}</p>
+    <button @click="loginFn1">同步操作</button>
+    <button @click="loginFn2">异步操作</button>
+  </div>
 </template>
 
 <script>
+
+
 export default {
-  name: 'myVuex',
-
-  mounted () {
-    console.log(this.$store)
+ 
+  mounted() {
+    console.log(this.$store);
   },
-
   methods: {
-    add() {
-      this.$store.commit('addNum',10)
+    globalFn1(){
+      this.$store.commit('changeGlobalName', 10);
     },
-    myFn2(){
-      this.$store.dispatch('asyncAddAge', 5);
+    globalFn2(){
+      this.$store.dispatch('asyncChangeGlobalName', 5);
+    },
+    homeFn1(){
+      this.$store.commit('changeHomeName', 10);
+    },
+    homeFn2(){
+      this.$store.dispatch('asyncChangeHomeName', 5);
+    },
+    accountFn1(){
+      this.$store.commit('changeAccountName', 10);
+    },
+    accountFn2(){
+      this.$store.dispatch('asyncChangeAccountName', 5);
+    },
+    loginFn1(){
+      this.$store.commit('changeLoginName', 10);
+    },
+    loginFn2(){
+      this.$store.dispatch('asyncChangeLoginName', 5);
     }
   }
 }
 </script>
 
 <style>
-
 </style>
--- helloWorld/src/store/index.js
+++ helloWorld/src/store/index.js
@@ -1,40 +1,136 @@
 import Vue from 'vue'
-// import Vuex from 'vuex'
-import Vuex from '../example/myvuex/index.js'
-
+import Vuex from 'vuex'
+// import Vuex from '../example/myvuex/index.js'
 Vue.use(Vuex)
-
-export default new Vuex.Store({
-  // 用于保存全局共享的数据
+let home = {
   state: {
-    name:'kebi',
-    num:0,
-    age: 0
+    name: 'www'
   },
   getters: {
-    myName(state){
-      return state.name + '666';
+    getHomeName(state){
+      return state.name + '222222';
+    },
+    // 多个模块中不能出现同名的getters方法
+    // getGlobalName(state){
+    //   return state.globalName + '111111';
+    // }
+  },
+  mutations: {
+    changeHomeName(state, payload){
+      state.name += payload;
+    },
+    // 注意点: 多个模块的mutations中可以出现同名的方法
+    //         多个同名的方法不会覆盖, 会放到数组中然后依次执行
+    changeGlobalName(state, payload){
+      console.log('home中的changeGlobalName');
+      state.globalName += payload;
+    }
+  },
+  actions: {
+    asyncChangeHomeName({commit}, payload){
+      setTimeout(()=>{
+        commit('changeHomeName', payload);
+      }, 1000);
+    },
+    // 注意点: 多个模块的actions中可以出现同名的方法
+    //         多个同名的方法不会覆盖, 会放到数组中然后依次执行
+    asyncChangeGlobalName({commit}, payload){
+      console.log('home中的asyncChangeGlobalName');
+      setTimeout(()=>{
+        commit('changeGlobalName', payload);
+      }, 1000);
+    }
+  }
+}
+let login = {
+  state: {
+    name: 'com'
+  },
+  getters: {
+    getLoginName(state){
+      return state.name + '333333';
+    }
+  },
+  mutations: {
+    changeLoginName(state, payload){
+      state.name += payload;
+    }
+  },
+  actions: {
+    asyncChangeLoginName({commit}, payload){
+      setTimeout(()=>{
+        commit('changeLoginName', payload);
+      }, 1000);
+    }
+  },
+}
+let account = {
+  state: {
+    name: 'it666'
+  },
+  getters: {
+    getAccountName(state){
+      return state.name + '333333';
+    }
+  },
+  mutations: {
+    changeAccountName(state, payload){
+      state.name += payload;
+    }
+  },
+  actions: {
+    asyncChangeAccountName({commit}, payload){
+      setTimeout(()=>{
+        commit('changeAccountName', payload);
+      }, 1000);
+    }
+  },
+  modules: {
+    login: login
+  }
+}
+export default new Vuex.Store({
+  /*
+  1.过去我们将所有模块的数据都放到state中共享, 但是这样会导致命名匮乏的问题
+  例如: 我们有三个模块 首页 / 个人中心 / 登录
+  首页中需要共享name, 个人中心中也需要共享name / 登录中也需要共享nanme
+  并且这三个name的取值还不一样, 那么为了能把这三条数据放到同一个state中, 我们就必须指定不同的名称
+  state:{
+    homeName: 'www',
+    accountName: 'it666',
+    loginName: 'com'
+  }
+  2.为了解决这个问题, Vuex就推出了模块化共享数据
+  通过模块化共享数据, 我们可以将不同模块共享的数据放到不同的state中
+  * */
+  // 用于保存全局共享数据
+  state: {
+    globalName: 'lnj'
+  },
+  getters: {
+    getGlobalName(state){
+      return state.globalName + '111111';
     }
   },
   // 用于同步修改共享数据
   mutations: {
-    addNum(state, payload){
-      console.log(state, payload);
-      state.num += payload;
-    },
-    addAge(state, payload){
-      state.age += payload;
+    changeGlobalName(state, payload){
+      console.log('全局中的changeGlobalName');
+      state.globalName += payload;
     }
   },
   // 用于异步修改共享数据
   actions: {
-    asyncAddAge({commit}, payload){
+    asyncChangeGlobalName({commit}, payload){
+      console.log('全局中的asyncChangeGlobalName');
       setTimeout(()=>{
-        commit('addAge', payload);
+        commit('changeGlobalName', payload);
       }, 1000);
     }
   },
-  // 用户模块化共享数据
+  // 用于模块化共享数据
   modules: {
+    home:home,
+    account: account
   }
 })
