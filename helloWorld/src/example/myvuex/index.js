e6d62b8f44bdbc59ea5643fd8347f9997ecf8601
keshanwen <624953505@qq.com>
2021-11-07 10:38:04 +0800

    feat:处理模块化信息

--- helloWorld/src/example/myvuex/index.js
+++ helloWorld/src/example/myvuex/index.js
@@ -1,121 +1,138 @@
 import Vue from 'vue'
-/* 
-install 方法会在外界调用Vue.use的时候执行
-并且在执行的时候会把Vue实例和一些额外的参数传递给我们
-
-*/
-
-const install = (Vue,options) => {
-  /* 给每一个vue实例都添加一个$store属性 */
-  /* 
-  在vue中有一个名称叫做mixin的方法，这个方法会在创建每一个vue实例的时候执行
-  所以我们可以通过mixin方法给每一个vue实例添加$store属性
-  
-  */
-  Vue.mixin({
-    beforeCreate () {
-      /* 
-      vue在创建实例的时候会先创建父组件，然后在创建子组件
-
-      root  >> app >> home
-
-      如果是根组件，那么默认就有store
-      我们只需要将store变成$store就可以
-      
-      */ 
-      //console.log(this.$options.name,'......')
-      if (this.$options && this.$options.store) {
-        this.$store = this.$options.store;
-      } else {
-        /* 
-          如果不是根组件，那么默认没有store
-          我们只需要将他父组件的$store赋值给他就可以
-        */
-         this.$store = this.$parent.$store
-      }
-
-    }
-  })
+const install = (Vue, options)=>{
+    // 给每一个Vue实例都添加一个$store属性
+    Vue.mixin({
+        beforeCreate(){
+            if(this.$options && this.$options.store){
+                this.$store = this.$options.store;
+            }
+            else{
+                this.$store = this.$parent.$store;
+            }
+        }
+    });
 }
-
+class ModuleCollection {
+    constructor(rootModule){
+        this.register([], rootModule);
+    }
+    register(arr, rootModule){
+        // console.log(arr); // [] [home] [account] [account, login]
+        // 1.按照我们需要的格式创建模块
+        let module = {
+            _raw: rootModule,
+            _state: rootModule.state,
+            _children: {}
+        }
+        // 2.保存模块信息
+        if(arr.length === 0){
+            // 保存根模块
+            this.root = module;
+        }else{
+            // 保存子模块
+            // this.root._children[arr[arr.length-1]] = module;
+            // let testArr = ['account', 'login'];
+            // let res = testArr.splice(0, testArr.length - 1);
+            // console.log(res);
+            // ['a', 'b', 'c'] -> ['a', 'b']
+            let parent = arr.splice(0, arr.length-1).reduce((root, currentKey)=>{
+                return root._children[currentKey];
+            }, this.root);
+            parent._children[arr[arr.length-1]] = module;
+        }
+        // 3.处理子模块
+        for(let childrenModuleName in rootModule.modules){
+            let childrenModule = rootModule.modules[childrenModuleName];
+            this.register(arr.concat(childrenModuleName) ,childrenModule)
+        }
+    }
+}
 class Store {
-  constructor(options) {
-    //this.options = options;
-    /* 
-      将创建的store时需要共享的数据添加到store上面
-      这样将来我们就能通过this.$store拿到这个store
-      既然能拿到这个store，我们就可以通过.state拿到需要共享的属性
-    */
-    //  this.state = options.state
-    /* 
-    
-    在vue中有一个util的工具类，这个工具类上有一个defineReactive方法
-    通过这个方法就可以快速的将某个数据变成全向绑定的数据
-    defineReactive这个方法接收三个参数
-        第一个参数: 要给哪个对象添加属性
-        第二个参数: 要给指定的对象添加什么属性
-        第三个参数: 要给这个属性添加什么值
-    */
-    Vue.util.defineReactive(this, 'state', options.state);
-     /* 
-     将传递进来的getters放到store上
-     */
-    //this.getters = options.getters;
-    this.initGetters(options)
-    /* 将传递进来的mutations放到store上 */
-    this.initMutations(options)
-    /* 将传递进来actions放到store上 */
-    this.intiActions(options)
-  }
-  initGetters(options) {
-    /* 
-      1,拿到传递进来的getters
-    */
-    let getters = options.getters || {}
-    /* 2,在store上新增一个getters属性 */
-    this.getters = {}
-    /* 3,将传递进来的getters中的方法添加到当前store的getters上 */
-    for (let key in getters) {
-     Object.defineProperty(this.getters,key,{
-       get: () => {
-         return getters[key](this.state)
-       }
-     })
+    constructor(options){
+        // 将传递进来的state放到Store上
+        Vue.util.defineReactive(this, 'state', options.state);
+        // 将传递进来的getters放到Store上
+        this.initGetters(options);
+        // 将传递进来的mutations放到Store上
+        this.initMutations(options);
+        // 将传递进来的actions放到Store上
+        this.initActions(options);
+        // 提取模块信息
+        this.modules = new ModuleCollection(options);
+        console.log(this.modules);
+        /*
+        let root = {
+            _raw: rootModule,
+            _state: rootModule.state,
+            _children: {
+                home:{
+                    _raw: homeModule,
+                    _state: homeModule.state,
+                    _children: {}
+                },
+                account:{
+                    _raw: accountModule,
+                    _state: accountModule.state,
+                    _children: {
+                        login: {
+                            _raw: loginModule,
+                            _state: loginModule.state,
+                            _children: {}
+                        }
+                    }
+                },
+            }
+        }
+        * */
     }
-  }
-  initMutations(options) {
-    // 1,拿到传递进来的mutations
-    let mutations = options.mutations || {}
-    // 2,在store上新增一个mutations属性
-    this.mutations = {}
-    // 3,将传递进来的mutations中的方法添加到当前store的mutations上
-    for (let key in mutations) {
-      this.mutations[key] = (playload) => {
-        mutations[key](this.state,playload)
-      }
+    dispatch = (type, payload)=>{ // 'asyncAddAge', 5
+        this.actions[type](payload); // this.actions[asyncAddAge](5);
     }
-  }
-  intiActions(options) {
-    // 1,拿到传递进来的ACTIONS
-    let actions = options.actions || {}
-    // 2，在store上新增一个actions的属性
-    this.actions = {}
-    // 3, 将传递进来的actions中的方法添加到当前store的actions上
-    for (let key in actions) {
-      this.actions[key] = (playload) => {
-          actions[key](this,playload)
-      }
+    initActions(options){
+        // 1.拿到传递进来的actions
+        let actions = options.actions || {};
+        // 2.在Store上新增一个actions的属性
+        this.actions = {};
+        // 3.将传递进来的actions中的方法添加到当前Store的actions上
+        for(let key in actions){
+            this.actions[key] = (payload)=>{ // 5
+                actions[key](this, payload); // asyncAddAge(this, 5);
+            }
+        }
     }
-  }
-  commit = (type,playload) => {
-    this.mutations[type](playload)
-  }
-  dispatch = (type,playload) => {
-    this.actions[type](playload)
-  }
+    commit = (type, payload)=>{ // 'addAge', 5
+        // console.log(this);
+        this.mutations[type](payload); //  this.mutations[addAge](5);
+    }
+    initMutations(options){
+        // 1.拿到传递进来的mutations
+        let mutations = options.mutations || {};
+        // 2.在Store上新增一个mutations的属性
+        this.mutations = {};
+        // 3.将传递进来的mutations中的方法添加到当前Store的mutations上
+        for(let key in mutations){
+            this.mutations[key] = (payload)=>{ // 10
+                mutations[key](this.state, payload); // addNum(this.state, 10);
+            }
+        }
+    }
+    initGetters(options){
+        // this.getters = options.getters;
+        // 1.拿到传递进来的getters
+        let getters = options.getters || {};
+        // 2.在Store上新增一个getters的属性
+        this.getters = {};
+        // 3.将传递进来的getters中的方法添加到当前Store的getters上
+        for(let key in getters){
+            Object.defineProperty(this.getters, key, {
+                get:()=>{
+                    return getters[key](this.state);
+                }
+            })
+        }
+    }
 }
-
 export default {
-  install,
-  Store
+    install,
+    Store
 }
--- helloWorld/src/store/index.js
+++ helloWorld/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-// import Vuex from '../example/myvuex/index.js'
+//import Vuex from 'vuex'
+import Vuex from '../example/myvuex/index.js'
 Vue.use(Vuex)
 let home = {
   state: {
