2c06ec85ec96fa8e74435145fa01ba971ce65f2f
keshanwen <624953505@qq.com>
2022-01-23 11:35:38 +0800

    收集依赖

--- vue3/reactivity/index.js
+++ vue3/reactivity/index.js
@@ -3,6 +3,28 @@ const convert = target => isObject(target) ? reactive(target) : target
 const hasOwnProperty = Object.prototype.hasOwnProperty
 const hasOwn = (target, key) => hasOwnProperty.call(target, key)
 
+let activeEffect = null
+function effect(callback) {
+  activeEffect = callback
+  callback() // 访问响应式对象属性，去收集依赖
+  activeEffect = null
+}
+
+let targetMap = new WeakMap()
+function track(target,key) {
+  if (!activeEffect) return
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target,( depsMap = new Map()))
+  } 
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+  dep.add(activeEffect)
+} 
+
+
 
 function reactive (target) {
   if (!isObject(target)) return target
@@ -10,6 +32,7 @@ function reactive (target) {
   const handler = {
     get (target, key, receiver) {
       // 收集依赖
+      track(target,key)
       const result = Reflect.get(target,key,receiver)
       return convert(result)
     },
@@ -35,4 +58,4 @@ function reactive (target) {
   return new Proxy(target,handler)
 }
 
-console.log(66666)
+
