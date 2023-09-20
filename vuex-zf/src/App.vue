394cc4c40a3513c2130db09ce0d711ae505307bf
keshanwen <3640@winnermedical.com>
2023-09-20 14:00:23 +0800

    feat: vuex 实现辅助函数

--- vuex-zf/src/App.vue
+++ vuex-zf/src/App.vue
@@ -1,53 +1,36 @@
 <template>
   <div id="app">
-    学校的年龄{{ this.$store.state.name }} {{ this.$store.state.age }} <br />
-    我的年龄是 {{ this.$store.getters.myAge }} 我的年龄是
-    {{ this.$store.getters.myAge }}
-  <button @click="$store.state.age++">非法更改状态</button>
-    <button @click="$store.commit('changeAge', 10)">更改年龄</button>
-    <button @click="$store.dispatch('changeAge', 10)">异步年龄</button>
-
-    <hr />
-
-    t1的年龄 {{ this.$store.state.a.name }} {{ this.$store.state.a.age }} <br />
-
-    t1的计算年龄 {{ this.$store.getters["a/myAge"] }}
-    <button @click="$store.commit('a/changeAge', 10)">t1更改年龄</button>
-    <hr />
-
-    c的年龄 {{ this.$store.state.a.c.age }}
-    <button @click="$store.commit('a/c/changeAge', 10)">t1更改年龄</button>
-
-    <hr />
-    <button @click="registerModule">动态注册模块</button>
-
-    <hr />
-    b模块 ({{ $store.state.b && $store.state.b.name }}
-    {{ $store.state.b && $store.state.b.age }})
-        {{ $store.getters.bAge && $store.getters.bAge }}
+    学校的年龄{{ name }} 我的年龄是 {{ myAge }}
+    <button @click="changeAge(10)">更改年龄</button>
+    <button @click="changeAgeAsync(10)">异步年龄</button>
   </div>
 </template>
 
-
-
 <script>
 import store from "./store";
+import { mapState, mapGetters, mapMutations, mapActions } from '@/vuex'
+
 export default {
-  name: "app",
   methods: {
-    registerModule() {
-      store.registerModule("b", {
-        state: {
-          name: "jiangwen",
-          age: 30,
-        },
-        getters:{
-          bAge(state){
-            return state.age+1;
-          }
-        }
-      });
-    },
+    ...mapMutations(["changeAge"]),
+    ...mapActions(["changeAge"])
+     // changeAge: MychangeAge
+    // changeAges(payload){
+    //   this.$store.commit('changeAge', payload)
+    // },
+    // changeAgeAsync(payload) {
+    //   this.$store.dispatch("changeAge", payload);
+    // },
+  },
+  computed: {
+   ...mapState(["name"]), // mapXxxx返回的是对象
+    ...mapGetters(["myAge"]),
+    // name(){
+    //   return this.$store.state.name
+    // },
+    // myAge(){
+    //   return this.$store.getters.myAge
+    // }
   },
 };
 </script>
--- vuex-zf/src/vuex/index.js
+++ vuex-zf/src/vuex/index.js
@@ -1,9 +1,47 @@
 import install from "./install";
 import Store from "./store";
 
+export function mapState(stateList) {
+    let obj = {};
+    for (let i = 0; i < stateList.length; i++) {
+      let stateName = stateList[i];
+      obj[stateName] = function () {
+        return this.$store.state[stateName];
+      };
+    }
+    return obj;
+  }
+ export function mapGetters(gettersList) {
+    let obj = {};
+    for (let i = 0; i < gettersList.length; i++) {
+      let getterName = gettersList[i];
+      obj[getterName] = function () {
+        return this.$store.getters[getterName];
+      };
+    }
+    return obj;
+  }
+  export function mapMutations(mutationList) {
+    let obj = {};
+    for (let i = 0; i < mutationList.length; i++) {
+      obj[mutationList[i]] = function (payload) {
+        this.$store.commit(mutationList[i], payload);
+      };
+    }
+    return obj;
+  }
+  export function mapActions(actionList) {
+    let obj = {};
+    for (let i = 0; i < actionList.length; i++) {
+      obj[actionList[i]] = function (payload) {
+        this.$store.dispatch(actionList[i], payload);
+      };
+    }
+    return obj;
+}
 
 
 export default {
   install,
-  Store
+  Store,
 }
